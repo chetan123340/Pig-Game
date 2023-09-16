821a6f780f3c24e1b000f7338aecd6f0ced5c611
Chetan <chetanrocks56@gmail.com>
2023-09-16 19:54:47 +0530

    game complete

--- game.py
+++ game.py
@@ -5,7 +5,6 @@ def roll():
   stop = 6
   return random.randint(start , stop)
 
-max_value = 50
 
 while True:
   player_no = input("Enter the number of players(2-5):- ")
@@ -19,20 +18,35 @@ while True:
     print("Invalid try again")
 
 
-player_score = [0 for _ in range(player_no)]
-player_index = 0
-while max(player_score) < 50:
-  print("\nPlayer ",player_index+1 , " turn has begun\n")
-  print("Total score = ", player_score[player_index])
-  score = 0
-  while True:
-    decision = input("Do you want to roll again(y/n): ")
-    if decision.islower != 'y':
-      break
- 
-    value = roll()
-    if value == 1:
-      print("You have rolled a 1. Your turn is over")
+player_scores = [0 for _ in range(player_no)]
+max_value = 50
 
+while max(player_scores) < max_value:
+  for player in range(player_no):
+    print("\nPlayer ",player+1 , " turn has begun\n")
+    print("Total score = ", player_scores[player])
+    score = 0
 
+    while True:
+      decision = input("Do you want to roll again(y/n): ")
+      if decision.lower() != 'y':
+        break
+  
+      value = roll()
+      if value == 1:
+        print("You have rolled a 1. Your turn is over")
+        score = 0
+        break
+      else:
+        score += value
+        print("You have rolled a ", value)
 
+      print("Current Score = ", score)
+
+    player_scores[player] += score
+    print("your total score is : ", player_scores[player])
+    
+max_score = max(player_scores)
+max_index = player_scores.index(max_score)
+
+print("Player ", max_index+1," has won with a score of ",max_score)
