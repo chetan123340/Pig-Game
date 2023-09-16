cb63fd94d6c71679d422bfb8938c246f5d4dd968
Chetan <chetanrocks56@gmail.com>
2023-09-16 19:19:03 +0530

    input player number

--- game.py
+++ game.py
@@ -5,4 +5,12 @@ def roll():
   stop = 6
   return random.randint(start , stop)
 
-print(roll())
+max_value = 50
+
+while True:
+  player_no = input("Enter the number of players(2-5):- ")
+  if player_no.isdigit():
+    if player_no<6 and player_no>=2:
+      break
+    else:
+      print("Player number should be between 2 to 5")
